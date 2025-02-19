21f9fc6b0f1b895c43d4f610f00e578ab06534d1
YSHUB <d.toshboltayev05@gmail.com>
2025-02-19 17:29:06 +0500

    model list added main photo fix

--- src/main/java/uz/carapp/rentcarapp/repository/AttachmentRepository.java
+++ src/main/java/uz/carapp/rentcarapp/repository/AttachmentRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.carapp.rentcarapp.domain.Attachment;
 
 public interface AttachmentRepository extends JpaRepository<Attachment, Integer> {
-    void getModelId(Long id);
 }
